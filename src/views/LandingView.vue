0a45e91c9115b9bbdb4450593d9daa18cb53a4bd
arthur <arthur.desmonts@gmail.com>
2025-02-05 11:47:31 +0100

    Setup :sparkles: : Remove useless Prebuild views | Add routes for LandingView and TestApi

---
+++ src/views/LandingView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Landing Page Ready!</h1>
+    </div>
+</template>
+
+<script>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    console.log('LandingPage mounted');
+});
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
