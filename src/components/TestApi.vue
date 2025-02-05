1d27b98fb8041ef6fafd11e6b8988c86e67e19f2
arthur <arthur.desmonts@gmail.com>
2025-02-05 11:00:57 +0100

    Setup : add a testAPI to control the good sharing of json datas LOCALY

---
+++ src/components/TestApi.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const message = ref('');
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('/test'); // Symfony doit avoir une route "/api/test"
+    message.value = response.data.message;
+  } catch (error) {
+    console.error("Erreur API :", error);
+  }
+});
+</script>
+
+<template>
+  <div>
+    <h1>Test API Symfony</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
