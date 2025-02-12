1f7fb00be0f8b7ce8dc981ee42410a34a09f3267
arthur <arthur.desmonts@gmail.com>
2025-02-12 18:07:57 +0100

    :white_check_mark: ADD : add the landing content of homePage

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,19 +1,55 @@
 <template>
-    <div class="flex h-screen">
-        <div id="titleFrame" class=" max-h-full w-1/2  p-4 flex items-center justify-center">
-            <h1 class="text-customBlack-100 text-center font-customInknut text-8xl">Control your wallet with Mywuc</h1>
-        </div>
-        <div id="widgetFrame" class="max-h-full w-1/2 p-4 flex items-center justify-center">
-            <div id="scareWidgetList" class="grid grid-cols-2 gap-4 w-2/4 h-2/4">
-                <span class="bg-customGrey-500 w-full h-full rounded-xl flex items-center justify-center">1</span>
-                <span class="bg-customGrey-500 w-full h-full rounded-xl flex items-center justify-center">2</span>
-                <span class="bg-customGrey-500 w-full h-full rounded-xl flex items-center justify-center">3</span>
-                <span class="bg-customGrey-500 w-full h-full rounded-xl flex items-center justify-center">4</span>
+    <div class="flex h-screen flex-col items-center justify-center">
+        <div class="flex w-full h-3/4 justify-between px-16">
+            <div id="titleFrame" class="w-2/5 flex items-center justify-center animate__fadeIn titleFrame">
+                <h1 class="text-customBlack-100 text-center font-customInknut text-6xl leading-tight">
+                    Control your wallet with Mywuc
+                </h1>
             </div>
+            <div class="w-1/2 flex justify-center animate__zoomIn ">
+                <img src="/wallet_homepage_picture.png" alt="Wallet" class="w-full max-w-[500px] h-auto object-contain">
+            </div>
+        </div>
+        
+        <div class="animate__fadeIn">
+            <hr class="w-full max-w-4xl border-t-2 border-customBlack-100 mb-2">
+
+            <div id="subTitle" class=" w-full max-w-4xl text-center ">
+                <h2 class="text-customBlack-100 font-customInknut text-4xl font-mono font-medium">
+                    Mywuc is a simple and intuitive way to manage your wallet
+                </h2>
+            </div>
+
+            <hr class="w-full max-w-4xl border-t-2 border-customBlack-100 mt-2">
         </div>
     </div>
 </template>
 
-<script>
 
-</script>
+<style scoped>
+    .animate__fadeIn {
+        animation: fadeIn 2s;
+    }
+
+    @keyframes fadeIn {
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+
+    .animate__zoomIn {
+        animation: zoomIn 1s;
+    }
+
+    @keyframes zoomIn {
+        0% {
+            transform: scale(0.8);
+        }
+        100% {
+            transform: scale(1);
+        }
+    }
+</style>
