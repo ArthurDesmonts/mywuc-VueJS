3ef38d89aa34ccacef969ac3416cd25939ed7611
arthur <arthur.desmonts@gmail.com>
2025-02-17 17:52:47 +0100

    :construction: signUp view clean : signUp implementation ready to code

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -4,12 +4,12 @@
             <div class="p-4 rounded bg-gray-300">
                 <h1 class="text-4xl font-bold text-center mb-2 font-customTeko">Sign Up</h1>
                 <form class="flex flex-col items-center justify-center">
-                    <input type="text" class="p-2 m-2 w-64 border border-gray-400 rounded" placeholder="Name">
-                    <input type="text" class="p-2 m-2 w-64 border border-gray-400 rounded" placeholder="First name">
-                    <input type="email" class="p-2 m-2 w-64 border border-gray-400 rounded" placeholder="Email">
-                    <input type="text" class="p-2 m-2 w-64 border border-gray-400 rounded" placeholder="Phone">
-                    <input type="password" class="p-2 m-2 w-64 border border-gray-400 rounded" placeholder="Password">
-                    <input type="password" class="p-2 m-2 w-64 border border-gray-400 rounded" placeholder="Confirm Password">
+                    <input ref="name" type="text" class="p-2 m-2 w-64 border border-gray-400 rounded" placeholder="Name">
+                    <input ref="firstName" type="text" class="p-2 m-2 w-64 border border-gray-400 rounded" placeholder="First name">
+                    <input ref="mail" type="email" class="p-2 m-2 w-64 border border-gray-400 rounded" placeholder="Email">
+                    <input ref="phone" type="text" class="p-2 m-2 w-64 border border-gray-400 rounded" placeholder="Phone">
+                    <input ref="pass" type="password" class="p-2 m-2 w-64 border border-gray-400 rounded" placeholder="Password">
+                    <input ref="passConf" type="password" class="p-2 m-2 w-64 border border-gray-400 rounded" placeholder="Confirm Password">
                     <button class="p-2 m-2 w-64 bg-blue-500 text-white rounded font-customPlayfair">Ready to start!</button>
                 </form>
             </div>
@@ -17,6 +17,18 @@
     </div>
 </template>
 
-<script>
-import { ref } from 'vue'
+<script setup>
+import { ref, onMounted } from 'vue';
+
+
+const name = ref(null);
+const firstName = ref(null);
+const mail = ref(null);
+const phone = ref(null);
+const pass = ref(null);
+const passConf = ref(null);
+
+//TODO : Implement the sign up logic
+
+
 </script>
