38e41406411a16d5523e7238b607cf6be3c859c6
arthur <arthur.desmonts@gmail.com>
2025-03-24 16:52:31 +0100

    :rotating_light: filtered transaction list properly working on sorting plan but :boom: not reactive any more on transaction adding

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,29 +4,34 @@
       <h2 class="text-2xl">My Dashboard :</h2>
       <div class="flex flex-row gap-8">
         <div v-if="user" class="ml-4 flex flex-row">
-            <div v-if="user.wallet" class=" border border-gray-300 rounded-md m-4 p-4 bg-green-100 text-green-800" :class="user.wallet.sold < 0 ? 'bg-red-100 text-red-800' : 'bg-green-100 text-green-800'">
-              <p class="text-lg font-semibold">Wallet Balance</p>
-              <p class="text-2xl">$ {{ user.wallet.sold}}</p>
-            </div>
-        </div>
-        <div v-if="user && user.Transactions && user.Transactions.length > 0" :class="lastTransaction.type === 'DEBIT' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'"
-            class="border border-gray-300 rounded-md m-4 p-4 bg-green-100 text-green-800">
-            <p class="text-lg font-semibold">Last Transaction :</p>
-            <p class="text-2xl text-center">$ {{ lastTransaction.amount }}</p>
-            <p class="text-sm text-gray-600">{{ shortedDate(lastTransaction.date) }}</p>
+          <div v-if="user.wallet" class="border border-gray-300 rounded-md m-4 p-4" :class="user.wallet.sold < 0 ? 'bg-red-100 text-red-800' : 'bg-green-100 text-green-800'">
+            <p class="text-lg font-semibold">Wallet Balance</p>
+            <p class="text-2xl">$ {{ user.wallet.sold }}</p>
           </div>
+        </div>
+        <div v-if="user && user.Transactions && user.Transactions.length > 0" :class="lastTransaction.type === 'DEBIT' ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'" class="border border-gray-300 rounded-md m-4 p-4">
+          <p class="text-lg font-semibold">Last Transaction :</p>
+          <p class="text-2xl text-center">$ {{ lastTransaction.amount }}</p>
+          <p class="text-sm text-gray-600">{{ shortedDate(lastTransaction.date) }}</p>
+        </div>
       </div>
     </div>
     <div class="flex flex-row mt-4 p-4 border-2 border-gray-300 m-4 rounded-2xl">
       <div v-if="user" class="m-4">
-        <p class="text-lg font-semibold">Transactions:</p>
-        <ul class="border border-gray-300 rounded-md p-2 bg-blue-100 text-blue-800">
-          <li v-for="transaction in user.Transactions" :key="transaction.id" 
-          :class="transaction.type === 'DEBIT' ? 'bg-green-100' : 'bg-red-100'"
-          class="mb-2 p-2 border-b border-gray-200 rounded-md">
-          <p class="font-medium">Type: {{ transaction.type }}</p>
-          <p>Amount: {{ transaction.amount }}</p>
-          <p class="text-sm text-gray-600">Date: {{ shortedDate(transaction.date) }}</p>
+        <div class="flex flex-row gap-1 justify-center">
+          <p class="text-lg font-semibold">Transactions:</p>
+          <select name="dateLimitation" id="dateLimitation" class="border border-gray-300 rounded-md p-1 bg-blue-100 text-blue-800 mb-2" @change="updateTransactions">
+            <option value="All">All Time</option>
+            <option value="LastMonth">Last Month</option>
+            <option value="LastWeek">Last Week</option>
+            <option value="Today">Today</option>
+          </select>
+        </div>
+        <ul class="border border-gray-300 rounded-md p-2 bg-blue-100 text-blue-800 overflow-y-auto" style="max-height: 290px;">
+          <li v-for="transaction in filteredTransactionList" :key="transaction.id" :class="transaction.type === 'DEBIT' ? 'bg-green-100' : 'bg-red-100'" class="mb-2 p-2 border-b border-gray-200 rounded-md">
+            <p class="font-medium">{{ transaction.type }}</p>
+            <p>Amount: {{ transaction.amount }}</p>
+            <p class="text-sm text-gray-600">Date: {{ shortedDate(transaction.date) }}</p>
           </li>
         </ul>
         <div class="flex flex-row gap-1 mt-1 justify-center">
@@ -51,6 +56,7 @@ import TransactionForm from '@/component/TransactionForm.vue';
 
 const store = useStore();
 const user = computed(() => store.state.user);
+
 const lastTransaction = computed(() => {
   if (user.value && user.value.Transactions && user.value.Transactions.length > 0) {
     return user.value.Transactions[user.value.Transactions.length - 1];
@@ -58,13 +64,22 @@ const lastTransaction = computed(() => {
   return null;
 });
 
+const transactionList = computed(() => {
+  if (user.value && user.value.Transactions) {
+    return user.value.Transactions;
+  }
+  return [];
+});
+
+const filteredTransactionList = ref([]);
 
 onMounted(async () => {
-try {
-  await store.dispatch('fetchUserProfile');
-} catch (err) {
-  console.error(err);
-}
+  try {
+    await store.dispatch('fetchUserProfile');
+    filteredTransactionList.value = transactionList.value;
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 /*
@@ -101,4 +116,25 @@ const shortedDate = (date) => {
   return `${dateReformated.getDate()}/${dateReformated.getMonth() + 1}/${dateReformated.getFullYear()}`;
 };
 
+function updateTransactions(event) {
+  let dateLimitation = event.target.value;
+  let dateLimit = new Date();
+
+  if (dateLimitation === 'All') {
+    filteredTransactionList.value = transactionList.value;
+  } else {
+    if (dateLimitation === 'LastMonth') {
+      dateLimit.setMonth(dateLimit.getMonth() - 1);
+    } else if (dateLimitation === 'LastWeek') {
+      dateLimit.setDate(dateLimit.getDate() - 7);
+    } else if (dateLimitation === 'Today') {
+      dateLimit.setHours(0, 0, 0, 0);
+    }
+
+    filteredTransactionList.value = transactionList.value.filter(transaction => {
+      let dateTransaction = new Date(transaction.date);
+      return dateTransaction >= dateLimit;
+    });
+  }
+}
 </script>
