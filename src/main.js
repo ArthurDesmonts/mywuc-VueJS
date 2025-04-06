4f3c49ff16230b4e5b15bac8a97a018f2d5ba5c5
arthur <arthur.desmonts@gmail.com>
2025-04-06 16:44:26 +0200

    server adress store change temp

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import store from './store'
 //API requires
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
+axios.defaults.baseURL = 'https://mywuc.onrender.com/api';
 
 const app = createApp(App)
 
